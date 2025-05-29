6c37c80670d8e8cfb25de79ff187f995ca27a751
18049261d@connect.polyu.hk <you@example.com>
2025-05-29 17:57:44 +0800

    preview joint positions b4 simulation

--- sim_test.py
+++ sim_test.py
@@ -4,6 +4,8 @@ import rospy
 import numpy as np
 import tf.transformations as tft
 import pandas as pd
+import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
 
 # filedescriptors = termios.tcgetattr(sys.stdin)
 # tty.setcbreak(sys.stdin)
@@ -24,7 +26,7 @@ controller = ambf_raven_controller.ambf_raven_controller()
 
 # Import data from dataset
 file_location = '/home/athena/Downloads/doi_10_5061_dryad_tqjq2bw84__v20241114/record_1_different_directions'
-file_name = 'data_record_yz_03.csv'
+file_name = 'data_record_x_03.csv'
 
 df = pd.read_csv(file_location+'/'+file_name) # Load CSV file
 
@@ -38,22 +40,26 @@ joint_vel_6 = df.iloc[:,167]
 joint_vel_7 = df.iloc[:,168]
 #print(joint_vel_1)
 
-# # Joint position desired
-# joint_pos_desired_1 = df.iloc[:,194]
-# joint_pos_desired_2 = df.iloc[:,195]
-# joint_pos_desired_3 = df.iloc[:,196]
-# N1 = len(joint_pos_desired_1)
-# N2 = len(joint_pos_desired_2)
-# N3 = len(joint_pos_desired_3)
+# Joint position desired
+joint_pos_desired_1 = df.iloc[:,194]
+joint_pos_desired_2 = df.iloc[:,195]
+joint_pos_desired_3 = df.iloc[:,196]
 # # print(N1, N2, N3)
 
-# # Joint position
-# joint_pos_1 = df.iloc[:,13]
-# joint_pos_2 = df.iloc[:,14]
-# joint_pos_3 = df.iloc[:,15]
-# n1 = len(joint_pos_1)
-# n2 = len(joint_pos_2)
-# n3 = len(joint_pos_3)
+# Joint position
+joint_pos_1 = df.iloc[:,13].values
+joint_pos_2 = df.iloc[:,14].values
+joint_pos_3 = df.iloc[:,15].values
+
+
+fig = plt.figure()
+ax = fig.add_subplot(111,projection='3d')
+ax.plot(joint_pos_1, joint_pos_2, joint_pos_3)
+plt.title('Test')
+ax.set_xlabel('Joint Position 1')
+ax.set_ylabel('Joint Position 2')
+ax.set_zlabel('Joint Position 3')
+plt.show()
 
 cmds = np.zeros((len(joint_vel_1),16))
 cmds[:,1] = np.deg2rad(joint_vel_1)
